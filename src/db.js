e31301b0071a5500d6e9bb914b090b15fb156287
Juan Racca <juanracca_@hotmail.com>
2022-07-29 12:04:52 -0300

    tratando de volver a localhost

--- src/db.js
+++ src/db.js
@@ -7,16 +7,16 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST
 } = process.env;
 
-const sequelize = new Sequelize(DB_HOST, DB_PASSWORD, DB_USER, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}:${DB_HOST}/pokemon`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    },
-  },
+  // dialect: 'postgres',
+  // dialectOptions: {
+  //   ssl: {
+  //     require: true,
+  //     rejectUnauthorized: false
+  //   },
+  // },
 });
 const basename = path.basename(__filename);
 
